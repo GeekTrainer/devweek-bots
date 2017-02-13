c6aa5af70eadfcfdc70001359fff549e38d84eb7
Christopher Harrison <chrhar@microsoft.com>
2017-02-13 12:18:54 -0800

    Initial commit

---
+++ bot.js
@@ -0,0 +1,23 @@
+const builder = require('botbuilder');
+
+const connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+const bot = new builder.UniversalBot(connector, [
+    (session, args, next) => {
+        const botName = 'Relay Bot';
+        const description = `Provides sample answers for a running relay`;
+
+        session.send(`Hi there! I'm ${botName}`);
+        session.send(`In a nutshell, here's what I can do:\n\n${description}`);
+
+        builder.Prompts.text(session, `What's your name?`);
+    },
+    (session, results, next) => {
+        session.endConversation(`Welcome, ${results.response}`);
+    }
+]);
+
+module.exports = bot;
