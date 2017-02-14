e0922155cae110ea81e8f0bfe9ef33156652821c
Christopher Harrison <chrhar@microsoft.com>
2017-02-14 09:38:02 -0800

    Added QnA

--- qadialog.js
+++ qadialog.js
@@ -15,7 +15,13 @@ module.exports = (session, args, next) => {
             session.endConversation('Sorry, something went wrong.');
         } else {
             const response = JSON.parse(body);
-
+            if(response.score > 50) {
+                session.endConversation(response.answer);
+            } else if(response.score > 0) {
+                session.endConversation(`I think this is what you're looking for:\n\n${response.answer}`);
+            } else {
+                session.endConversation(`I'm sorry, I don't have the answer for that.`);
+            }
         }
     }).setHeader('Ocp-Apim-Subscription-Key', process.env.SUBSCRIPTION_KEY);
 };
